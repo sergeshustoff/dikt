a6649c2d5320e3a79c4550af686a4b279c5c6064
serge shustoff <serge.shustoff@gmail.com>
2021-01-30 22:27:29 +0300

    refactoring; add runtime lib dependency from plugin

--- dikt-gradle-plugin/src/main/kotlin/dev/shustoff/dikt/gradle/DiktGradlePlugin.kt
+++ dikt-gradle-plugin/src/main/kotlin/dev/shustoff/dikt/gradle/DiktGradlePlugin.kt
@@ -1,6 +1,8 @@
 package dev.shustoff.dikt.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.DependencyResolutionListener
+import org.gradle.api.artifacts.ResolvableDependencies
 import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
@@ -11,6 +13,9 @@ class DiktGradlePlugin : KotlinCompilerPluginSupportPlugin {
 
     override fun apply(target: Project) {
         target.extensions.create("dikt", DiktGradleExtension::class.java)
+        target.configurations.filter { it.name.endsWith("implementation", ignoreCase = true) }.forEach {
+            target.dependencies.add(it.name, target.dependencies.create("io.github.sergeshustoff:dikt-runtime:$version"))
+        }
     }
 
     override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
@@ -19,20 +24,26 @@ class DiktGradlePlugin : KotlinCompilerPluginSupportPlugin {
 
     override fun getCompilerPluginId(): String = "io.github.sergeshustoff.dikt"
 
-    override fun getPluginArtifact(): SubpluginArtifact = SubpluginArtifact(
-        groupId = "io.github.sergeshustoff",
-        artifactId = "dikt-compiler-plugin",
-        version = "1.0.0-alpha1"
-    )
+    override fun getPluginArtifact(): SubpluginArtifact {
+        return SubpluginArtifact(
+            groupId = "io.github.sergeshustoff",
+            artifactId = "dikt-compiler-plugin",
+            version = version
+        )
+    }
 
     override fun getPluginArtifactForNative(): SubpluginArtifact = SubpluginArtifact(
         groupId = "io.github.sergeshustoff",
         artifactId = "dikt-compiler-plugin-native",
-        version = "1.0.0-alpha1"
+        version = version
     )
 
     override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean {
         return kotlinCompilation.target.project.plugins.hasPlugin(DiktGradlePlugin::class.java)
+    }
+
+    companion object {
+        private const val version = "1.0.0-alpha1"
     }
 }
 
