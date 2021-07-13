31c96be32d6be3a59d49c1a432822f0f6f6f2d0e
serge shustoff <serge.shustoff@gmail.com>
2021-07-13 19:04:15 +0300

    improve error logging

--- dikt-compiler-plugin-shared/src/main/kotlin/dev/shustoff/dikt/message_collector/ErrorCollector.kt
+++ dikt-compiler-plugin-shared/src/main/kotlin/dev/shustoff/dikt/message_collector/ErrorCollector.kt
@@ -1,12 +1,14 @@
 package dev.shustoff.dikt.message_collector
 
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationWithName
-import org.jetbrains.kotlin.ir.util.getPackageFragment
-import java.util.*
+import org.jetbrains.kotlin.ir.declarations.path
+import org.jetbrains.kotlin.ir.util.file
+import org.jetbrains.kotlin.ir.util.fileEntry
 
 interface ErrorCollector {
     fun IrDeclarationWithName?.info(text: String)
@@ -18,41 +20,28 @@ interface ErrorCollector {
 private class ErrorCollectorImpl(
     private val messageCollector: MessageCollector
 ): ErrorCollector {
-    override fun error(text: String) {
-        messageCollector.report(CompilerMessageSeverity.ERROR, text, null)
+    override fun IrDeclarationWithName?.error(text: String) {
+        messageCollector.report(CompilerMessageSeverity.ERROR, text, location())
     }
 
-    override fun IrDeclarationWithName?.error(text: String) {
-        messageCollector.report(
-            CompilerMessageSeverity.ERROR,
-            fullName()?.let { "$it: $text" } ?: text,
-        )
+    override fun error(text: String) {
+        messageCollector.report(CompilerMessageSeverity.ERROR, text)
     }
 
     override fun IrDeclarationWithName?.info(text: String) {
-        messageCollector.report(
-            CompilerMessageSeverity.WARNING,
-            fullName()?.let { "$it: $text" } ?: text,
-        )
+        messageCollector.report(CompilerMessageSeverity.WARNING, text, location())
     }
 
     override fun info(text: String) {
         messageCollector.report(CompilerMessageSeverity.WARNING, text)
     }
 
-    private fun IrDeclarationWithName?.fullName(): String? {
-        var node = this
-        val nodes = LinkedList<String>()
-        while (node != null) {
-            nodes.add(0, node.name.asString())
-            val parent = node.parent as? IrDeclarationWithName
-            if (parent == null) {
-                node.getPackageFragment()?.fqName?.asString()?.let { nodes.add(0, it) }
-            }
-            node = parent
-        }
-
-        return nodes.takeUnless { it.isEmpty() }?.joinToString(separator = ".")
+    private fun IrDeclarationWithName?.location() = this?.let {
+        CompilerMessageLocation.create(it.file.path,
+            it.fileEntry.getLineNumber(it.startOffset),
+            it.fileEntry.getColumnNumber(it.startOffset),
+            null
+        )
     }
 }
 
