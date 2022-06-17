b68f691348a7e33d26d9a958f871561814c33c6f
serge shustoff <serge.shustoff@gmail.com>
2022-06-17 10:21:07 +0200

    set version to 1.0.2

--- dikt-gradle-plugin/src/main/kotlin/dev/shustoff/dikt/gradle/DiktGradlePlugin.kt
+++ dikt-gradle-plugin/src/main/kotlin/dev/shustoff/dikt/gradle/DiktGradlePlugin.kt
@@ -37,7 +37,7 @@ class DiktGradlePlugin : KotlinCompilerPluginSupportPlugin {
     }
 
     companion object {
-        private const val version = "1.0.2-alpha1"
+        private const val version = "1.0.2"
     }
 }
 
